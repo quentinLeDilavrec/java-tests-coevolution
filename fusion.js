7c0c9811e853fafcc4fed785c435405c692101d3
quentin <ledilavrecquentin@gmail.com>
2020-07-20 00:45:47 +0200

    input list

--- fusion.js
+++ fusion.js
@@ -10,17 +10,21 @@ const rev = revT.split('\n')
     .map(x => x.split(' '))
     .map(x => [x[0], { stars: parseInt(x[1]), revs: x.slice(2) }])
 
-rev.forEach(x => (maxS = Math.max(x[1].stars, maxS), fusion[x[0]] = { ...x[1] }))
+rev.forEach(x => (maxS = Math.max(x[1].stars||0, maxS), fusion[x[0]] = { ...x[1] }))
 
 const top = topT.split('\n').slice(1)
     .map(x => x.split(','))
     .map(x => [x[0], { loc: parseInt(x[1]), forks: parseInt(x[2]), watchers: parseInt(x[3]) }])
 
-top.forEach(x => (maxF = Math.max(x[1].forks, maxF), maxL = Math.max(x[1].loc, maxL), maxW = Math.max(x[1].watchers, maxW), fusion[x[0]] = { ...fusion[x[0]], ...x[1] }))
+top.forEach(x => (maxF = Math.max(x[1].forks||0, maxF), maxL = Math.max(x[1].loc||0, maxL), maxW = Math.max(x[1].watchers||0, maxW), fusion[x[0]] = { ...fusion[x[0]], ...x[1] }))
 
-console.log(fusion)
+// console.log(fusion)
+console.log(maxS,maxF,maxW,maxL)
 
-Object.entries(fusion).map(x => (x[1].weight = ((x[1].stars || 0) / maxS * 10) + ((x[1].forks || 0) / maxF) + ((x[1].watchers) || 0 / maxW), x))
-    .sort((a, b) => a[1].weight - b[1].weight)
-    .forEach(x => console.log(x))
+Object.entries(fusion)
+.filter(x=>x[1].revs&&x[1].revs.length>1)
+.map(x => (x[1].weight = ((x[1].stars || 0) / maxS*10) + ((x[1].forks || 0) / maxF) + ((x[1].watchers) || 0 / maxW*10), x))
+    .sort((b, a) => a[1].weight - b[1].weight)
+    // .slice(0,10)
+    .forEach(x => console.log(x[0]+" "+x[1].weight+" "+x[1].revs.join(" ")))
 //.forEach(x=>console.log(x[0],x[1].stars,x[1].forks,x[1].watcher,x[1].loc))
